3b5f6fd47feec7d92546264bf7e348d6d622c068
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2024-02-17 17:28:47 +1000

    gnu plot didnt seem like the best solution
    one option is to use matplotlib. but i need to check that i can run python alright

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include "../include/Scene.h"
-#include "../include/gnuplot_i.hpp"
-
 
 int main() {
     
@@ -9,22 +7,5 @@ int main() {
     opticalBench.trace();
     opticalBench.printRays();
     opticalBench.printShape();
-
-    Gnuplot plotter;
-
-    // Set plot options
-    plotter.cmd("set title '3D Vector Plot'");
-    plotter.cmd("set xlabel 'X'");
-    plotter.cmd("set ylabel 'Y'");
-    plotter.cmd("set zlabel 'Z'");
-
-    // Define 3D vector data in the format expected by plot_xyz
-    std::vector<double> x{1.0, 4.0};
-    std::vector<double> y{2.0, 5.0};
-    std::vector<double> z{3.0, 6.0};
-
-    // Plot 3D vectors using plot_xyz
-    plotter.plot_xyz(x, y, z, "Vectors");
-    
     return 0;
 };
