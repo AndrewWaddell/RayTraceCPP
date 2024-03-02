24fc2f9169180e47dc164ab165def7fe0cd41c52
AndrewWaddell <106304665+AndrewWaddell@users.noreply.github.com>
2024-03-02 12:51:04 +1000

    added broadcast to add()
    decided to use recursion instead of creating a new function because then we don't have to assume the matrices have the same dimensions with only a small addition of code.

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -399,11 +399,19 @@ void Matrix::multiplyPointwise(Matrix A,Matrix B){
 
 void Matrix::add(Matrix inMat){
     // assume both matrices have the same dimensions
-    for (int i=0;i<numRows;i++){
-        for (int j=0;j<numCols;j++){
-            matrix[i][j] += inMat.get(i,j);
+    if (inMat.numCols == numCols){
+        if (inMat.numRows == numRows){
+            for (int i=0;i<numRows;i++){
+                for (int j=0;j<numCols;j++){
+                    matrix[i][j] += inMat.get(i,j);
+                }
+            }
         }
+    } else if (inMat.numRows == numRows){ // inMat is col vector adding to every column of this
+        inMat.broadcast(numCols);
+        add(inMat);
     }
+    
 };
 
 void Matrix::subtract(Matrix A,Matrix B){
